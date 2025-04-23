92d7b71a4411899c209040c9f9c1c42d96fa5d8e
hanupratap <hanupratap1999@gmail.com>
2025-04-22 22:18:31 -0700

    Fixing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import styled from '@emotion/styled';
 import { motion } from 'framer-motion';
-import { keyframes } from '@emotion/react';
 import '@fontsource-variable/urbanist';
 import '@fontsource-variable/eb-garamond';
 
